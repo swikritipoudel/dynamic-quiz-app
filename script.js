b2a9e603e0de2ee447a78680d8fd00bd534a3ea9
Swikriti Poudel <poudelswikriti76@gmail.com>
2025-06-25 19:13:20 +0545

    Added some basic html and javascript code

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
+let category = document.getElementById("category").value
+let difficulty= document.getElementById("difficultyLevel").value
+let quizForm = document.querySelector(".quizForm")
+let card = document.querySelector("card")
 
+quizForm.addEventListener("submit",async function (event){
+    event.preventDefault()
 
+    let nextQuestion = document.createElement("button")
+    nextQuestion.textContent = "Next Question"
+    card.appendChild(nextQuestion)
+    
+    
+    const apiUrl = `https://opentdb.com/api.php?amount=30&category=${category}&difficulty=${difficulty}&type=multiple`
+
+    let response = await fetch(apiUrl)
+    
+
+    try{
+    let data = await response.json()
+    console.log(data)
+        }
+
+    catch(error){
+            console.error(error)
+    }
+
+    
+})
